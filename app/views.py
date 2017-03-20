9590403dbcf3f77772a24b34d6bb9501bcf41027
voron434 <070809010@mail.ru>
2017-03-20 22:02:20 +0300

    healing print

--- app/views.py
+++ app/views.py
@@ -100,14 +100,14 @@ def index():
         return render_template('index.html', **params)
     online_friends_ids = online_friends_ids['response']
     
-    params['test'] = online_friends_ids
-    return render_template('index.html', **params)
-    
     pc_online_friends_info = get_users_info(token, online_friends_ids['online'])
     if 'error' in pc_online_friends_info:
         params['error'] = error_healing(pc_online_friends_info['error']['error_code'])
         return render_template('index.html', **params)
     
+    params['test'] = pc_online_friends_info
+    return render_template('index.html', **params)
+
     telephone_online_friends_info =get_users_info(token, online_friends_ids['online_mobile'])
     if 'error' in telephone_online_friends_info:
         params['error'] = error_healing(telephone_online_friends_info['error']['error_code'])
