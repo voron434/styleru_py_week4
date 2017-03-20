47fa33b658cd467b6048a236589ab856f041b261
voron434 <070809010@mail.ru>
2017-03-20 20:05:59 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
-#!flask/bin/python
+from argparse import ArgumentParser
 from app import app
+import os
+
+
+def parse_argv():
+    parser = ArgumentParser(description="Starts flask server.")
+    parser.add_argument('--host', '-H', type=str, default="0.0.0.0",
+                        help="Specify host id.")
+    parser.add_argument('--port', '-p', type=int, default=int(os.environ.get('PORT', 5000)),
+                        help="Specify local port, where to start server.")
+    parser.add_argument('--debug', '-d', type=bool, default=True,
+                        help="Start server in debug mode.")
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    args = parse_argv()
+    app.run(host=args.host, port=args.port, debug=args.debug)
