365ed831e09340801316b69d2ade59da1e0230b9
voron434 <070809010@mail.ru>
2017-03-20 22:32:08 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -104,10 +104,7 @@ def index():
     if 'error' in pc_online_friends_info:
         params['error'] = error_healing(pc_online_friends_info['error']['error_code'])
         return render_template('index.html', **params)
-
-    params['test'] = pc_online_friends_info
-    return render_template('index.html', **params)
-
+    
     telephone_online_friends_info =get_users_info(token, online_friends_ids['online_mobile'])
     if 'error' in telephone_online_friends_info:
         params['error'] = error_healing(telephone_online_friends_info['error']['error_code'])
