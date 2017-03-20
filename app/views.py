8407947ec6339bc7390e52e6ce53bbed3b93e2f2
voron434 <070809010@mail.ru>
2017-03-20 23:34:50 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,12 +1,84 @@
 from flask import render_template, flash, redirect, request, session
 from app import app
-from vk_helpers import error_healing,form_url,get_user_info,get_online_friends_ids
+import vk_helpers
 import requests
 import json
+import time
 import os
 
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
+
+
+def error_healing(error_code):
+    if error_code == 1:
+        return 'Произошла неизвестная ошибка'
+    elif error_code == 2:
+        return 'Сорян, админ все повыключал'
+    elif error_code == 5:
+        return 'Авторизация не удалась'
+    elif error_code == 6:
+        time.sleep(2)
+        return None
+    elif error_code == 9:
+        return 'Слишком много однотипных действий'
+    elif error_code == 14:
+        return 'Прости, вылезла капча. Попробуй перезайти'
+    elif error_code == 15:
+        return 'Этот юзер спрятался от меня'
+    elif error_code == 17:
+        return 'Так исторически сложилось, что тебе придется войти'
+    elif error_code == 18:
+        return 'Эта страничка удалена, у нее нет друзей'
+    elif error_code == 113:
+        return 'Прости, но ты ввел что-то не так, как я ожидаю'
+    elif error_code == 1000:
+        return 'Нет, сначала положи что-нибудь в форму!'
+    else:
+        return 'Тебе повезло! Ты нашел новую ошибку!'
+
+
+
+def form_url(redirect_uri):
+    params = {'client_id': CLIENT_ID,
+              'display': 'page',
+              'redirect_uri': redirect_uri,
+              'scope': 'friends',
+              'response_type': 'code',
+              'v': '5.62',
+              }
+    request = requests.Request('GET', 'https://oauth.vk.com/authorize',
+                               params=params)
+    request.prepare()
+    return request.prepare().url
+
+
+def get_user_info(token, short_name):
+    params = {'user_ids': short_name,
+              'access_token': token,
+              }
+    url = 'https://api.vk.com/method/users.get'
+    request = json.loads(requests.get(url, params).text)
+    return request
+
+
+def get_online_friends_ids(short_name, token):
+    if not short_name:
+        return {'error': {'error_code': 1000}}
+
+    user_info = get_user_info(token, short_name)
+    if 'error' in user_info:
+        return user_info
+    url = 'https://api.vk.com/method/friends.getOnline'
+    params = {'user_id': user_info['response'][0]['uid'],
+              'access_token': token,
+              'order': 'hints',
+              'count': 5000,  # vk won't return more
+              'v': '5.62',
+              'online_mobile': 1,
+              }
+    vk_friends_online = json.loads(requests.get(url, params).text)
+    return vk_friends_online
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -14,7 +86,7 @@ CLIENT_SECRET = os.environ['CLIENT_SECRET']
 def index():
 
     params = {'logged_in': False,
-              'auth_url': form_url(CLIENT_ID, request.url_root + 'getpas'),
+              'auth_url': form_url(request.url_root + 'getpas'),
               'logout_url': '/logout'
               }
     short_name = request.args.get('text', '')
