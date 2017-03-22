e0e8bfb498b1532a0f3cc2e33efd47c528d3220d
voron434 <070809010@mail.ru>
2017-03-23 00:12:32 +0300

    refactoring

--- app/views.py
+++ app/views.py
@@ -1,122 +1,46 @@
 from flask import render_template, flash, redirect, request, session
 from app import app
 import requests
-import json
-import time
 import os
+from . import vk_helpers
+
 
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
 
 
-def error_healing(error_code):
-    if error_code == 1:
-        return 'Произошла неизвестная ошибка'
-    elif error_code == 2:
-        return 'Сорян, админ все повыключал'
-    elif error_code == 5:
-        return 'Авторизация не удалась'
-    elif error_code == 6:
-        time.sleep(2)
-        return None
-    elif error_code == 9:
-        return 'Слишком много однотипных действий'
-    elif error_code == 14:
-        return 'Прости, вылезла капча. Попробуй перезайти'
-    elif error_code == 15:
-        return 'Этот юзер спрятался от меня'
-    elif error_code == 17:
-        return 'Так исторически сложилось, что тебе придется войти'
-    elif error_code == 18:
-        return 'Эта страничка удалена, у нее нет друзей'
-    elif error_code == 113:
-        return 'Прости, но ты ввел что-то не так, как я ожидаю'
-    elif error_code == 1000:
-        return 'Нет, сначала положи что-нибудь в форму!'
-    else:
-        return 'Тебе повезло! Ты нашел новую ошибку!'
-
-
-
-def form_url(redirect_uri):
-    params = {'client_id': CLIENT_ID,
-              'display': 'page',
-              'redirect_uri': redirect_uri,
-              'scope': 'friends',
-              'response_type': 'code',
-              'v': '5.62',
-              }
-    request = requests.Request('GET', 'https://oauth.vk.com/authorize',
-                               params=params)
-    request.prepare()
-    return request.prepare().url
-
-
-def get_user_info(token, short_name):
-    params = {'user_ids': short_name,
-              'access_token': token,
-              }
-    url = 'https://api.vk.com/method/users.get'
-    request = json.loads(requests.get(url, params).text)
-    return request
-
-
-def get_online_friends_ids(short_name, token):
-    if not short_name:
-        return {'error': {'error_code': 1000}}
-
-    user_info = get_user_info(token, short_name)
-    if 'error' in user_info:
-        return user_info
-    url = 'https://api.vk.com/method/friends.getOnline'
-    params = {'user_id': user_info['response'][0]['uid'],
-              'access_token': token,
-              'order': 'hints',
-              'count': 5000,  # vk won't return more
-              'v': '5.62',
-              'online_mobile': 1,
-              }
-    vk_friends_online = json.loads(requests.get(url, params).text)
-    return vk_friends_online
+def is_error_there(response, **params):
+    flag = False
+    if 'error' in response:
+        flag = True
+        params['error'] = vk_helpers.show_error(response['error']['error_code'])
+    return flag
 
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-
     params = {'logged_in': False,
-              'auth_url': form_url(request.url_root + 'getpas'),
+              'auth_url': vk_helpers.form_url(CLIENT_ID, request.url_root + 'getpas'),
               'logout_url': '/logout'
               }
     short_name = request.args.get('text', '')
     if 'access_token' not in session:
         return render_template('index.html', **params)
+    else:
+        params['logged_in'] = True
+        token = session['access_token']
 
-    params['logged_in'] = True
-    token = session['access_token']
-    online_friends_ids = get_online_friends_ids(short_name, token)
-    if 'error' in online_friends_ids:
-        params['error'] = error_healing(online_friends_ids['error']['error_code'])
+    online_friends_ids = vk_helpers.get_all_friends_ids(short_name, token)
+    if is_error_there(online_friends_ids, **params):
         return render_template('index.html', **params)
-    online_friends_ids = online_friends_ids['response']
-
-    friends_info_pc = []
-    for friend_id in online_friends_ids['online']:
-        friend_info = get_user_info(token, friend_id)
-        if 'error' in friend_info:
-            error_healing(friend_info['error']['error_code'])
-            friend_info = get_user_info(token, friend_id)
-        friends_info_pc.append(friend_info)
-    friends_info_mobile = []
-    for friend_id in online_friends_ids['online_mobile']:
-        friend_info = get_user_info(token, friend_id)
-        if 'error' in friend_info:
-            error_healing(friend_info['error']['error_code'])
-            friend_info = get_user_info(token, friend_id)
-        friends_info_mobile.append(friend_info)
-    params['online_friends_mobile'] = friends_info_mobile
-    params['online_friends_pc'] = friends_info_pc
-    params.pop('online_friends', None)
+    else:
+        online_friends_ids = online_friends_ids['response']['items']
+    online_friends = []
+    for friend in online_friends_ids:
+        if friend['online']:
+            online_friends.append(friend)
+    params['online_friends'] = online_friends
     return render_template('index.html', **params)
 
 
@@ -141,5 +65,5 @@ def getpas():
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
-    session.pop('access_token',None)
+    session.pop('access_token', None)
     return redirect('index')
